9a19fd11fc005a1c5cbda0b76a5d0a4f765a25ac
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-10-31 12:41:04 +0100

    Add User model

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace Mde.Project.Mobile.Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PassWord { get; set; }
+        public int MidWifePhoneNumber { get; set; }
+        public Location Location { get; set; }
+    }
+}
