5be85c46aa523c4ca312d3b1984baa4ba835b7ec
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-10-31 14:07:00 +0100

    Add IMotherService

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/IMotherService.cs
@@ -0,0 +1,15 @@
+﻿using Mde.Project.Mobile.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Domain.Services
+{
+    public interface IMotherService
+    {
+        Task<Mother> GetMother(Guid id);
+        Task<Mother> UpdateMother(Mother mother);
+        Task<Mother> CreateMother(Mother mother);
+    }
+}
