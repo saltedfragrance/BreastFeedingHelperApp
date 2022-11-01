6baafc25b645c1b6dde6910d9889fb5dc47f04a4
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-11-01 11:34:41 +0100

    Add TimeLinePage and TimeLineViewModel

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/TimeLineViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.ViewModels
+{
+    public class TimeLineViewModel : FreshBasePageModel
+    {
+    }
+}
