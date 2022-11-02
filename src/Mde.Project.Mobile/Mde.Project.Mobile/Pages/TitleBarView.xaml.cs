b93edf9232fac9b298718dc6f092bd135cf55aee
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-11-02 21:10:51 +0100

    Move page title bar to reusable view

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/TitleBarView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mde.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TitleBarView : ContentView
+    {
+        public TitleBarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
