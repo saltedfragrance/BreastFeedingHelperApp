549e54ccdb5d3f526628bad6abf85ccf2ac61bc9
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-11-02 13:32:56 +0100

    Create basic BabyPage and BabyViewModel

--- src/Mde.Project.Mobile/Mde.Project.Mobile/App.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/App.xaml.cs
@@ -23,7 +23,7 @@ namespace Mde.Project.Mobile
             mainContainer.BarBackgroundColor = Color.Pink;
             mainContainer.AddTab<TimeLineViewModel>("Timeline", "timeline.png", null);
             mainContainer.AddTab<TimeLineViewModel>("Statistics", "statistics.png", null);
-            mainContainer.AddTab<TimeLineViewModel>("Babies", "baby.png", null);
+            mainContainer.AddTab<BabyViewModel>("Babies", "baby.png", null);
             mainContainer.AddTab<TimeLineViewModel>("Breastfeeding", "breastfeeding.png", null);
             mainContainer.AddTab<TimeLineViewModel>("Memories", "memories.png", null);
             mainContainer.On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
