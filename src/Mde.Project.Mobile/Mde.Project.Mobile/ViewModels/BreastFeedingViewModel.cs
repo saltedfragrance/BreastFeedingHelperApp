f7c5fa34bdd6e506c1bce0b301c64550d6fb31f2
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-11-05 21:09:16 +0100

    Add ReverseInit to BreastFeedingViewModel

--- src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/BreastFeedingViewModel.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/BreastFeedingViewModel.cs
@@ -7,9 +7,16 @@ namespace Mde.Project.Mobile.ViewModels
 {
     public class BreastFeedingViewModel : FreshBasePageModel
     {
+        public bool IsBreastFeedingPage { get; set; } = true;
         protected async override void ViewIsAppearing(object sender, EventArgs e)
         {
             base.ViewIsAppearing(sender, e);
+        }
+
+        public override void ReverseInit(object returnedData)
+        {
+            base.ReverseInit(returnedData);
+            IsBreastFeedingPage = false;
         }
     }
 }
