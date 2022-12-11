25e950b56049d16388586edc313640d9cb1c101c
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-11 16:04:11 +0100

    Add FirebasePushNotificationManager to mainactivity

--- src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainActivity.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.FirebasePushNotification;
 
 namespace Mde.Project.Mobile.Droid
 {
@@ -17,6 +18,8 @@ namespace Mde.Project.Mobile.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            FirebasePushNotificationManager.ProcessIntent(this, Intent);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
