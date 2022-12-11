a2111e1c8e0d35e4931a0eeb685132cd3923f8ca
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-11 16:42:09 +0100

    Add support for push notifications in MainApplication

--- src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainApplication.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainApplication.cs
@@ -23,6 +23,7 @@ public class MainApplication : Application
 
             //Change for your default notification channel name here
             FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+            FirebasePushNotificationManager.DefaultNotificationChannelImportance = NotificationImportance.Max;
         }
 
         //If debug you should reset the token each time.
