27f1bb6ae826415f19ffff33c3c0dca60567c68a
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-15 19:46:46 +0100

    Add IMemoryService and MemoryService

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/Interfaces/IMemoryService.cs
@@ -0,0 +1,14 @@
+﻿using Mde.Project.Mobile.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Domain.Services.Interfaces
+{
+    public interface IMemoryService
+    {
+        Task CreateMemory(string title, string description, string date, string mediaUri, int motherId);
+        Task DeleteMemory(string id);
+    }
+}
---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/MemoryService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Services
+{
+    public class MemoryService
+    {
+    }
+}
