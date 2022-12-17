d1362a3b75243c7e4cc0a856ab588cfa683277f4
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-17 12:54:45 +0100

    Add add memory page

--- src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/AddMemoryViewModel.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/AddMemoryViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using FreshMvvm;
 using Mde.Project.Mobile.Domain.Enums;
 using Mde.Project.Mobile.Domain.Models;
+using Mde.Project.Mobile.Domain.Services;
 using Mde.Project.Mobile.Domain.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.CommunityToolkit.Core;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Mde.Project.Mobile.ViewModels
@@ -32,6 +37,50 @@ namespace Mde.Project.Mobile.ViewModels
             {
                 pageTitle = value;
                 RaisePropertyChanged(nameof(pageTitle));
+            }
+        }
+
+        private bool isPicture;
+        public bool IsPicture
+        {
+            get { return isPicture; }
+            set
+            {
+                isPicture = value;
+                RaisePropertyChanged(nameof(IsPicture));
+            }
+        }
+
+        private bool isMovie;
+        public bool IsMovie
+        {
+            get { return isMovie; }
+            set
+            {
+                isMovie = value;
+                RaisePropertyChanged(nameof(IsMovie));
+            }
+        }
+
+        private ImageSource pictureSource;
+        public ImageSource PictureSource
+        {
+            get { return pictureSource; }
+            set
+            {
+                pictureSource = value;
+                RaisePropertyChanged(nameof(PictureSource));
+            }
+        }
+
+        private MediaSource videoSource;
+        public MediaSource VideoSource
+        {
+            get { return videoSource; }
+            set
+            {
+                videoSource = value;
+                RaisePropertyChanged(nameof(VideoSource));
             }
         }
 
@@ -96,6 +145,74 @@ namespace Mde.Project.Mobile.ViewModels
             {
                 await CoreMethods.PopPageModel(true, true);
             });
+        public ICommand AddPicture => new Command(
+        async () =>
+        {
+            Stream stream = null;
+            IsPicture = true;
+            IsMovie = false;
+            VideoSource= null;
+
+            var result = await MediaPicker.PickPhotoAsync();
+            if (result != null) stream = await result.OpenReadAsync();
+            else return;
+
+            PictureSource = ImageSource.FromStream(() => stream);
+        });
+
+
+        public ICommand TakePicture => new Command(
+            async () =>
+            {
+                IsPicture = true;
+                IsMovie = false;
+                VideoSource = null;
+
+                Stream stream = null;
+                var result = await MediaPicker.CapturePhotoAsync();
+                if (result != null) stream = await result.OpenReadAsync();
+                else return;
+                PictureSource = ImageSource.FromStream(() => stream);
+            });
+
+        public ICommand AddMovie => new Command(
+            async () =>
+            {
+                //https://stackoverflow.com/questions/71583611/xamarin-forms-mediaelement-playing-video-selected-from-gallery-with-crossmedia
+                
+                IsPicture = false;
+                IsMovie = true;
+                PictureSource = null;
+
+                string path = string.Empty;
+                var fileName = "tempvideo";
+                var newFile = Path.Combine(FileSystem.AppDataDirectory, fileName + ".mp4");
+                Stream stream = null;
+                var result = await MediaPicker.PickVideoAsync();
+                if (result != null) stream = await result.OpenReadAsync();
+                else return;
+
+                using (var newStream = File.OpenWrite(newFile))
+                    await stream.CopyToAsync(newStream);
+
+                VideoSource = MediaSource.FromFile(newFile);
+            });
+
+        public ICommand FilmMovie => new Command(
+            async () =>
+            {
+                IsPicture = false;
+                IsMovie = true;
+                PictureSource = null;
+
+                Stream stream = null;
+                var result = await MediaPicker.CaptureVideoAsync();
+                if (result != null) stream = await result.OpenReadAsync();
+                else return;
+
+                PictureSource = ImageSource.FromStream(() => stream);
+            });
+
 
         //public ICommand AddMemory => new Command(
         //    async () =>
