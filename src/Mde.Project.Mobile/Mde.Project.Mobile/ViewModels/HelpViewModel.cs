223e3e89fd94572ac5b71ce28cc2e542f3a8f49b
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-18 14:19:55 +0100

    Add phone to midwife functionality

--- src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/HelpViewModel.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/HelpViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using FreshMvvm;
+using Mde.Project.Mobile.Domain.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace Mde.Project.Mobile.ViewModels
 {
     public class HelpViewModel : FreshBasePageModel
     {
+        private readonly IMotherService _motherService;
+        public HelpViewModel(IMotherService motherService)
+        {
+            _motherService = motherService;
+        }
+
         private string pageTitle;
         public string PageTitle
         {
@@ -17,11 +27,25 @@ namespace Mde.Project.Mobile.ViewModels
                 RaisePropertyChanged(nameof(PageTitle));
             }
         }
-
         protected async override void ViewIsAppearing(object sender, EventArgs e)
         {
             PageTitle = "Help";
             base.ViewIsAppearing(sender, e);
         }
+
+        public ICommand PhoneToMidWife => new Command(
+            async () =>
+            {
+                try
+                {
+                    PhoneDialer.Open(_motherService.CurrentMother.MidWifePhoneNumber.ToString());
+                }
+                catch (Exception)
+                {
+
+                    var phoneNumber = await CurrentPage.DisplayPromptAsync("Something went wrong", "Please enter a valid number", "Ok,", "Cancel");
+                    PhoneDialer.Open(phoneNumber);
+                }
+            });
     }
 }
