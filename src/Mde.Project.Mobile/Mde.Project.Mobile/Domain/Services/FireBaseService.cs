b15503077ce201f0851f296a0a800c9ad384c806
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-22 20:16:49 +0100

    Change firebase config

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/FireBaseService.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/FireBaseService.cs
@@ -10,10 +10,10 @@ namespace Mde.Project.Mobile.Domain.Services
 {
     public class FireBaseService : IFireBaseService
     {
-        public FirebaseClient Client { get; } = new FirebaseClient("https://babytracker-9df68-default-rtdb.europe-west1.firebasedatabase.app/");
-        public FirebaseAuthProvider AuthProvider { get; } = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyCwbYQx5eBLQU4ZCC6OTXyuOpwkS0iSlvM"));
+        public FirebaseClient Client { get; } = new FirebaseClient("https://baby-a38ca-default-rtdb.europe-west1.firebasedatabase.app/");
+        public FirebaseAuthProvider AuthProvider { get; } = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyC6vKhpDe2-g1DfDsf_t9uFJqHnAmPxkQ0 "));
 
-        public FirebaseStorage FireBaseStorage { get; } = new FirebaseStorage("babytracker-9df68.appspot.com",
+        public FirebaseStorage FireBaseStorage { get; } = new FirebaseStorage("baby-a38ca.appspot.com",
                 new FirebaseStorageOptions
                 {
                     ThrowOnCancel = true,
