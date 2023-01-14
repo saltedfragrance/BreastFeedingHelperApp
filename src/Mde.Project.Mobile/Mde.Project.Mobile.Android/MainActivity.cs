343c52d500417bb8924c01961833adc6cd595e7b
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2023-01-14 15:15:38 +0100

    Change app icons and splash screen

--- src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainActivity.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Acr.UserDialogs;
 
 namespace Mde.Project.Mobile.Droid
 {
-    [Activity(Label = "BabyTracker", Icon = "@mipmap/babyicon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "BabyTracker", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
