785a4a7e77b8d48dc5900e31ab59e91ca5de115c
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2023-01-14 13:50:26 +0100

    Disable landscape mode

--- src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainActivity.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Acr.UserDialogs;
 
 namespace Mde.Project.Mobile.Droid
 {
-    [Activity(Label = "Mde.Project.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Mde.Project.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
